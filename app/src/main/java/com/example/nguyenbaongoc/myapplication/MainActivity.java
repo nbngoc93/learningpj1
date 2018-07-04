39fb33fd0519da5fb7ce407bd4ee06aa18d354c5
nbngoc <nbngoc93@gmail.com>
2018-07-04 14:31:09 +0700

    init

---
+++ app/src/main/java/com/example/nguyenbaongoc/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.nguyenbaongoc.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onForgotpass(View view) {
+
+    }
+
+    public void onCreatacc(View view) {
+
+    }
+
+    public void onVerify(View view) {
+
+    }
+
+    public void onBtlogin(View view) {
+    }
+
+    public void onBtoffline(View view) {
+    }
+}
